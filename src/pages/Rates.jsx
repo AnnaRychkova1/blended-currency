114ae609139448b1ff105f1aaeb6d7b3a44d6796
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2024-04-20 13:27:14 +0300

    task 8

--- src/pages/Rates.jsx
+++ src/pages/Rates.jsx
@@ -1,12 +1,20 @@
 import { Wave } from 'react-animated-text';
 
-import { Container, Heading, Loader, RatesList, Section } from 'components';
+import {
+  Container,
+  Filter,
+  Heading,
+  Loader,
+  RatesList,
+  Section,
+} from 'components';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import {
   selectBaseCurrency,
   selectIsError,
   selectIsLoading,
+  selectRates,
 } from 'reduxState/selectors';
 import { fetchLatestRates } from 'reduxState/currency/currencyOperations';
 import { selectFilteredRates } from 'reduxState/selectors';
@@ -15,7 +23,8 @@ const Rates = () => {
   const dispatch = useDispatch();
   const isError = useSelector(selectIsError);
   const isLoading = useSelector(selectIsLoading);
-  const rates = useSelector(selectFilteredRates);
+  const filteredRates = useSelector(selectFilteredRates);
+  const rates = useSelector(selectRates);
   const baseCurrency = useSelector(selectBaseCurrency);
 
   useEffect(() => {
@@ -36,7 +45,8 @@ const Rates = () => {
             />
           }
         />
-        {rates.length > 0 && <RatesList rates={rates} />}
+        {rates.length > 0 && <Filter />}
+        {filteredRates.length > 0 && <RatesList rates={filteredRates} />}
 
         {isError && (
           <Heading
