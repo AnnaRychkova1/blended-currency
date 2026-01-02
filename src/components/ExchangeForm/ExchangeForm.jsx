2996dc274311be663e612191c8a90401cba13adb
Anna Rychkova <aanytkaa@gmail.com>
2026-01-02 13:53:15 +0000

    favicon+placeholder

--- src/components/ExchangeForm/ExchangeForm.jsx
+++ src/components/ExchangeForm/ExchangeForm.jsx
@@ -1,11 +1,13 @@
 import { RiExchangeDollarFill } from 'react-icons/ri';
 import styles from './ExchangeForm.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectBaseCurrency } from 'reduxState/selectors';
 
 import { fetchExchange } from 'reduxState/currency/currencyOperations';
 
 export const ExchangeForm = () => {
   const dispatch = useDispatch();
+  const baseCurrency = useSelector(selectBaseCurrency);
 
   const handleSubmit = evt => {
     evt.preventDefault();
@@ -19,10 +21,10 @@ export const ExchangeForm = () => {
       </button>
 
       <input
-        title="Request format 15 USD in UAH"
+        title={`Request format: 15 USD in ${baseCurrency}`}
         className={styles.input}
-        placeholder="15 USD in UAH"
-        pattern="^\d+(\.\d{1,2})?\s[a-zA-Z]{3}\sin\s[a-zA-Z]{3}$"
+        placeholder={`15 USD in ${baseCurrency}`}
+        pattern="^\d+(\.\d{1,2})?\s[A-Za-z]{3}\sin\s[A-Za-z]{3}$"
         name="currency"
         required
       />
